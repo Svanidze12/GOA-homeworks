dc1ec7334e8ebe36e6ff9adbf6f04b7455b74b7a
Svanidze12 <androsvanidze2008@gmail.com>
2024-03-14 14:13:50 +0400

    commit

---
+++ Day 05/homework.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first numbere here: "))
+num2 = int(input("Enter second numbere here: "))
+
+
+#gavaketot matematikuri moqmedebebi
+
+print(num1 + num2 == num1 + num2)
+print(num1 * num2 > num1 + num2)
+print(num1 - num2 < num1 + num2)
+print(num1 / num2 < num1 + num2)
