a096bf2eeee3c0e53ce871ebc03db00587752f72
Svanidze12 <androsvanidze2008@gmail.com>
2024-03-17 23:19:20 +0400

    commit

--- Day 1/homework.py
+++
@@ -1,7 +0,0 @@
-name = "andria"
-surname = "svanidze"
-age = 15
-
-#printeverything
-
-print("my name is" + " " + name + " " + "my surname is" + surname + " " + "and" + " " + "i am" + " " + str(age) + " " + " " + "years old")
